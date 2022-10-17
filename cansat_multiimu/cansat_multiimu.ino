389d48caaa81d1bdfff36dcb0d9259809a3d6929
Laura Train <60850007+lauratrain99@users.noreply.github.com>
2022-10-17 20:25:12 +0200

    include two buses

--- cansat_multiimu/cansat_multiimu.ino
+++ cansat_multiimu/cansat_multiimu.ino
@@ -3,6 +3,9 @@
 #include <Wire.h>
 
 File myFile;
+const int SDA_2 = 26;
+const int SCL_2 = 25;
+
 const int MPU2 = 0x69, MPU1=0x68;
 const int MPU3 = 0x69, MPU4=0x68;
 short accelX, accelY, accelZ;
@@ -12,6 +15,7 @@ float rotX, rotY, rotZ;
 float gForceX2, gForceY2, gForceZ2;
 unsigned long mytime;
 
+char buf[40];
 
 void setup() {
   
@@ -20,14 +24,14 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  //Serial.print("Initializing SD card...");
-  if (!SD.begin(10)) {
+  Serial.print("Initializing SD card...");
+  /*if (!SD.begin(10)) {
     Serial.println("initialization failed!");
     while (1);
   }
   Serial.println("initialization done.");
 
-  myFile = SD.open("ELE.txt", FILE_WRITE);
+  myFile = SD.open("ELE.txt", FILE_WRITE);*/
   
   Wire.begin();
   Wire.beginTransmission(MPU1);
@@ -57,7 +61,7 @@ void setup() {
   Wire.write(0b00000000);
   Wire.endTransmission(); 
 
-  Wire1.begin();
+  Wire1.begin(SDA_2, SCL_2, 100000);
   Wire1.beginTransmission(MPU3);
   Wire1.write(0x6B);
   Wire1.write(0b00000000);
@@ -71,7 +75,7 @@ void setup() {
   Wire1.write(0b00000000);
   Wire1.endTransmission(); 
 
-  Wire1.begin();
+  Wire1.begin(SDA_2, SCL_2, 100000);
   Wire1.beginTransmission(MPU4);
   Wire1.write(0x6B);
   Wire1.write(0b00000000); 
@@ -85,63 +89,52 @@ void setup() {
   Wire1.write(0b00000000);
   Wire1.endTransmission(); 
 
+  if(!SD.begin()){
+        Serial.println("Card Mount Failed");
+        return;
+  }
+
+  
+  writeFile(SD, "/log.txt", "Init file imu log: \n");
+  
+
 }
 void loop() {
 // nothing happens after setup
   
   
   Serial.print("IMU1 | ");
-  //myFile.print("IMU1 | ");
-  //Serial.print("T = ");
-  //myFile.print("T = ");
-  //Serial.print(millis()/1000.0, 4);
-  //myFile.print(millis()/1000.0,4);
-  //Serial.print(" ");
-  //myFile.print(" ");
+  appendFile(SD, "/log.txt", "IMU1 | ");
+  appendFile(SD, "/log.txt", "T = ");
+  sprintf(buf, "%.4f", millis()/1000.0);
+  appendFile(SD, "/log.txt", buf );
 
   GetMpuValue1(MPU1);
-  Serial.println("");
+  appendFile(SD, "/log.txt", "\n");
 
-  Serial.print("IMU2 | ");
-  myFile.print("IMU2 | ");
-  Serial.print("T = ");
-  myFile.print("T = ");
-  Serial.print(millis()/1000.0, 4);
-  myFile.print(millis()/1000.0,4);
-  Serial.print(" ");
-  myFile.print(" ");
+  appendFile(SD, "/log.txt", "IMU2 | ");
+  appendFile(SD, "/log.txt", "T = ");
+  sprintf(buf, "%.4f", millis()/1000.0);
+  appendFile(SD, "/log.txt", buf );
   
   GetMpuValue1(MPU2);
-  Serial.println("");
+  appendFile(SD, "/log.txt", "\n");
 
-  Serial.print("IMU3 | ");
-  myFile.print("IMU3 | ");
-  Serial.print("T = ");
-  myFile.print("T = ");
-  Serial.print(millis()/1000.0, 4);
-  myFile.print(millis()/1000.0,4);
-  Serial.print(" ");
-  myFile.print(" ");
-  
+  appendFile(SD, "/log.txt", "IMU3 | ");
+  appendFile(SD, "/log.txt", "T = ");
+  sprintf(buf, "%.4f", millis()/1000.0);
+  appendFile(SD, "/log.txt", buf );
+
   GetMpuValue2(MPU3);
-  Serial.println("");
-
-  Serial.print("IMU4 | ");
-  myFile.print("IMU4 | ");
-  Serial.print("T = ");
-  myFile.print("T = ");
-  Serial.print(millis()/1000.0, 4);
-  myFile.print(millis()/1000.0,4);
-  Serial.print(" ");
-  myFile.print(" ");
+  appendFile(SD, "/log.txt", "\n");
+  
+  appendFile(SD, "/log.txt", "IMU4 | ");
+  appendFile(SD, "/log.txt", "T = ");
+  sprintf(buf, "%.4f", millis()/1000.0);
+  appendFile(SD, "/log.txt", buf );
   
   GetMpuValue2(MPU4);
-  Serial.println("");
-
-  //if (mytime > 5000.0F)
-  //{
-    //myFile.close();
-  //}*/
+  appendFile(SD, "/log.txt", "\n");
 }
 
 void GetMpuValue1(const int MPU){
@@ -171,36 +164,45 @@ void GetMpuValue1(const int MPU){
   rotY = (float)gyroY / 131.0; 
   rotZ = (float)gyroZ / 131.0;
 
-  if (myFile){
-    Serial.print("ANG_VEL[deg/s]: ");
-    myFile.print("ANG_VEL[deg/s]: ");
-    Serial.print(rotX,5);
-    myFile.print(rotX,5);
-    Serial.print("; ");
-    myFile.print("; ");
-    Serial.print(rotY,5);
-    myFile.print(rotY,5);
-    Serial.print("; ");
-    myFile.print("; ");
-    Serial.print(rotZ,5);
-    myFile.print(rotZ,5);
-    Serial.print("; ");
-    myFile.print("; ");
-    Serial.print("\t ACC[g]: ");
-    myFile.print("\t ACC[g]: ");
-    Serial.print(gForceX,5);
-    myFile.print(gForceX,5);
-    Serial.print("; ");
-    myFile.print("; ");
-    Serial.print(gForceY,5);
-    myFile.print(gForceY,5);
-    Serial.print("; ");
-    myFile.print("; ");
-    Serial.print(gForceZ,5);
-    myFile.print("; ");
-  }
+    Serial.print(" ANG_VEL[deg/s]: ");
+    appendFile(SD, "/log.txt", " ANG_VEL[deg/s]: ");
   
-  delay(100);
+    Serial.print(rotX,5);
+    sprintf(buf, "%.4f", rotX);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+    
+    Serial.print(rotY,5);
+    sprintf(buf, "%.4f", rotY);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+    
+    Serial.print(rotZ,5);
+    sprintf(buf, "%.4f", rotZ);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+
+    Serial.print("ACC[g]: ");
+    appendFile(SD, "/log.txt", "ACC[g]: ");
+
+    Serial.print(gForceX,5);
+    sprintf(buf, "%.4f", gForceX);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+    
+    Serial.print(gForceY,5);
+    sprintf(buf, "%.4f", gForceY);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+
+    Serial.print(gForceZ,5);
+    sprintf(buf, "%.4f", gForceZ);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+    
+    appendFile(SD, "/log.txt", "\n");
+    
+  delay(25);
 
 }
 
@@ -229,36 +231,79 @@ void GetMpuValue2(const int MPU){
   rotX = gyroX / 131.0;
   rotY = gyroY / 131.0; 
   rotZ = gyroZ / 131.0;
-
-  if (myFile){
-    Serial.print("ANG_VEL[deg/s]: ");
-    myFile.print("ANG_VEL[deg/s]: ");
+  
+    Serial.print(" ANG_VEL[deg/s]: ");
+    appendFile(SD, "/log.txt", " ANG_VEL[deg/s]: ");
+  
     Serial.print(rotX,5);
-    myFile.print(rotX,5);
-    Serial.print("; ");
-    myFile.print("; ");
+    sprintf(buf, "%.4f", rotX);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+    
     Serial.print(rotY,5);
-    myFile.print(rotY,5);
-    Serial.print("; ");
-    myFile.print("; ");
+    sprintf(buf, "%.4f", rotY);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+    
     Serial.print(rotZ,5);
-    myFile.print(rotZ,5);
-    Serial.print("; ");
-    myFile.print("; ");
-    Serial.print("\t ACC[g]: ");
-    myFile.print("\t ACC[g]: ");
+    sprintf(buf, "%.4f", rotZ);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+
+    Serial.print("ACC[g]: ");
+    appendFile(SD, "/log.txt", "ACC[g]: ");
+
     Serial.print(gForceX,5);
-    myFile.print(gForceX,5);
-    Serial.print("; ");
-    myFile.print("; ");
+    sprintf(buf, "%.4f", gForceX);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+    
     Serial.print(gForceY,5);
-    myFile.print(gForceY,5);
-    Serial.print("; ");
-    myFile.print("; ");
+    sprintf(buf, "%.4f", gForceY);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+
     Serial.print(gForceZ,5);
-    myFile.print("; ");
-  }
+    sprintf(buf, "%.4f", gForceZ);
+    appendFile(SD, "/log.txt", buf );
+    appendFile(SD, "/log.txt", "; ");
+    
+    appendFile(SD, "/log.txt", "\n");
+    
   
   delay(100);
 
+}
+
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+    file.close();
 }
